e5c32379defe1dd1ac3d5f26f5453e9867649a7d
Suwi-inc <suwilanjisilwamba@gmail.com>
2024-02-20 23:28:15 +0300

    add russian and latin language support

--- WindowsClipSolver/Core/ExtractText.cs
+++ WindowsClipSolver/Core/ExtractText.cs
@@ -13,15 +13,14 @@ namespace WindowsClipSolver.Core
     internal class ExtractText
     {
      
-        public string getText(string image)
+        public string getText(Bitmap image)
         {
             var text = "";
             try
             {
-                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                using (var engine = new TesseractEngine(@"./tessdata", "eng+rus+lat", EngineMode.Default))
                 {
-                    //Pix.LoadFromMemory(new ImageConverter().ConvertTo(image, typeof(byte[])) as byte[])
-                    using (var img = Pix.LoadFromFile(image))
+                    using (var img = Pix.LoadFromMemory(new ImageConverter().ConvertTo(image, typeof(byte[])) as byte[]))
                     {
                         using (var page = engine.Process(img))
                         {
